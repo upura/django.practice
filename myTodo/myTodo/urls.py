7b79760d2ebdec334bddc8d0b2261b9a50d34207
upura <upura0@gmail.com>
2018-04-20 16:53:53 +0900

    temporary commit

--- myTodo/myTodo/urls.py
+++ myTodo/myTodo/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+import myTodo.views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', myTodo.views.index, name='index'),
 ]
