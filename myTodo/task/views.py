72885c246500fb9d34f185c5a7d6dc80dcacadf9
upura <upura0@gmail.com>
2018-04-24 16:46:17 +0900

    Get name, text, created_at from Task

--- myTodo/task/views.py
+++ myTodo/task/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.template.response import TemplateResponse
 from django.http import Http404
 from task.models import Task
-
+import pdb
 def index(request):
-    # Get tasks
     tasks = list(Task.objects.all())
     return TemplateResponse(request, 'task/index.html',
                             {'tasks': tasks})
@@ -12,6 +11,7 @@ def index(request):
 def task_detail(request, task_id):
     try:
         task = Task.objects.get(id=task_id)
+#        pdb.set_trace()
     except Task.DoesNotExist:
         raise Http404
     return TemplateResponse(request, 'task/task_detail.html',
